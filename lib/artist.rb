15257be14ee52737decba85e30333931aba300c8
Sara A <calisaraalhindi@gmail.com>
2020-08-27 16:14:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,15 @@
 class Artist
   attr_accessor :name
-  
+ 
   def initialize(name)
     @name = name
-    @songs = []
   end
-  
+ 
   def add_song(song)
-    @songs << song
     song.artist = self
-    end
-    
-    def songs
-      Song.all.select {|song| song.artist == self}
-    end
-    
-    end
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+end
